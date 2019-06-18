a9b4f6cbcb65b555dbc65f46e37160d63eaa94ce
Melissa <jstmac@gmail.com>
2019-06-17 21:39:56 -0600

    added iniMiddleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./providers/AuthProvider";
 import "semantic-ui-css/semantic.min.css";
+import { initMiddleware } from "devise-axios";
+
+// will take axios requests and will look for any sort of authorization header - what your token will be saved as in local storage.
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
