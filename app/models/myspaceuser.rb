805116aa2101700352dda00abd0e6fe361bdb54d
Melissa <jstmac@gmail.com>
2019-06-18 21:36:59 -0600

    added profiles account controller

--- app/models/myspaceuser.rb
+++
@@ -1,2 +0,0 @@
-class Myspaceuser < ApplicationRecord
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
+
+  namespace :api do
+    resources :profiles, only: [:index, :update]
+    get "my_profiles", to: "profiles#my_profiles"
+  end
 end
+
+
 
