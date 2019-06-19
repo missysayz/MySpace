b7b748d5d3173a6d74ada6337d0c3bd0c0551fa6
Melissa <jstmac@gmail.com>
2019-06-18 21:46:14 -0600

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  name = Faker::Name.name,
+  dob = Faker::Date.birthday(18, 65),
+  avatar = Faker::Avatar.image(name, '100x400', 'png', 'set4'),
+  Profile.create(name: name, dob: dob, avatar: avatar)
+end
+
+puts "50 Profiles Seeded"
