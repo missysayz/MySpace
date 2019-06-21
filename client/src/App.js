41ec29544fbcc20ab23598c04b4ca1fd009d010b
Melissa <jstmac@gmail.com>
2019-06-21 16:53:36 -0600

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,39 @@ import Home from "./components/Home.js";
 import NoMatch from "./components/NoMatch";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
+import ProtectedRoute from './components/ProtectedRoute';
 import Register from "./components/Register";
 import { Switch, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import FetchUser from "./components/FetchUser";
 import MyProfiles from "./components/MyProfiles";
+import Profile from './components/Profile'
 import MyProfileForm from './components/MyProfileForm';
-import MyFriends from "./components/MyFriends";
+// import MyFriends from "./components/MyFriends";
 // import AuthProvider from "./providers/AuthProvider";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => (
   <Fragment>
-    <div>
-      <Navbar />
-      <FetchUser>
-        <Container>
-          <Switch>
-            <ProtectedRoute exact path='/' component={Home} />
-            <ProtectedRoute exact path='/profile/my-friends' component={MyFriends} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/MyProfileForm' component={MyProfileForm} />
-            <ProtectedRoute exact path='/my_profiles' component={MyProfiles} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Container>
-      </FetchUser>
-    </div>
-    ;
+
+    <Navbar />
+    <FetchUser>
+      <Container>
+        <Switch>
+          <ProtectedRoute exact path='/' component={Home} />
+          <ProtectedRoute exact path='/profiles' component={Profile} />
+          <ProtectedRoute exact path='/my_profiles' component={MyProfiles} />
+          <ProtectedRoute exact path="/friends/:id" component={Friend} />
+
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/MyProfileForm' component={MyProfileForm} />
+
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
   </Fragment>
 );
 
