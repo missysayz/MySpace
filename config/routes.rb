a09719d5cf5c6c83ad1c363bd10eb587e7c4432a
Melissa <jstmac@gmail.com>
2019-06-21 21:24:14 -0600

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
 
   namespace :api do
-    resources :profiles, only: [:index, :create, :update]
+    resources :profiles, only: [:index, :create, :update, :show]
     get "my_profiles", to: "profiles#my_profiles"
-    get "/my_profile", to: "profiles#my_profile"
   end
-
-  resources :profiles do
-    resources :friends, only: [:index]
-    get "my_friends", to: "friends#my_friends"
-end
-
 
 end
 
