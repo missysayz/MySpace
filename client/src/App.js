89596077cb9396fd9481ed703f86b608e415e29c
Melissa <jstmac@gmail.com>
2019-06-21 22:15:28 -0600

    Edited App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from "react";
 import Home from "./components/Home.js";
-import NoMatch from "./components/NoMatch";
 import Navbar from "./components/Navbar";
+import NoMatch from "./components/NoMatch";
 import Login from "./components/Login";
-import ProtectedRoute from './components/ProtectedRoute';
 import Register from "./components/Register";
-import { Switch, Route } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import ProtectedRoute from './components/ProtectedRoute';
 import FetchUser from "./components/FetchUser";
 import MyProfiles from "./components/MyProfiles";
 import Profile from './components/Profile'
-import MyProfileForm from './components/MyProfileForm';
+import { Container } from "semantic-ui-react";
+import { Switch, Route } from "react-router-dom";
+
+// import MyProfileForm from './components/MyProfileForm';
 // import MyFriends from "./components/MyFriends";
 // import AuthProvider from "./providers/AuthProvider";
 
@@ -22,14 +23,12 @@ const App = () => (
       <Container>
         <Switch>
           <ProtectedRoute exact path='/' component={Home} />
-          <ProtectedRoute exact path='/profiles' component={Profile} />
           <ProtectedRoute exact path='/my_profiles' component={MyProfiles} />
+          <ProtectedRoute exact path='/profiles' component={Profile} />
           {/* <ProtectedRoute exact path="/friends/:id" component={Friend} /> */}
-
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
-          <Route exact path='/MyProfileForm' component={MyProfileForm} />
-
+          {/* <Route exact path='/MyProfileForm' component={MyProfileForm} /> */}
           <Route component={NoMatch} />
         </Switch>
       </Container>
