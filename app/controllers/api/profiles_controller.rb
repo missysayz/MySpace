ab0e9fa688e74f9ca44a5d7111f806ca5bf39c69
Melissa <jstmac@gmail.com>
2019-06-21 21:36:34 -0600

    profiles controller

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -3,41 +3,39 @@ class Api::ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: User.random_profiles(current_user.follow_profiles)
+    render json: User.random_profiles
+    (current_user.follow_profiles)
     
   end
 
-  def create
-    profile = current_user.profiles.new(profile_params)
-    if profile.save
-      render json: profile
-    else
-      render json: {}
-    end
-  end
+  # def create
+  #   profile = current_user.profiles.new(profile_params)
+  #   if profile.save
+  #     render json: profile
+  #   else
+  #     render json: {}
+  #   end
+  # end
 
   def update
-    if current_user.follow_profiles << params[:id].to_i 
-      current_user.profile.save
-    else 
-      render json: {}
+    current_user.follow_profiles << params[:id].to_i 
+    current_user.save
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+    render json: @profile
+  end
+
+  def my_profiles
+    render json: User.follow(current_user.follow_profiles)
   end
 end
 
-def show
-  @profile = Profile.find(params[:id])
-  render json: @profile
-end
-
-def my_profiles
-  render json: User.follow(current_user.follow_profiles)
-end
-
-  private
-    def profile_params
-      params.require(:profile).permit(:name, :dob, :avatar)
-    end
-  def my_profiles  
-    render json: User.profile(user_id: current_user.id)
-  end
-end
+  # private
+  #   def profile_params
+  #     params.require(:profile).permit(:name, :dob, :avatar)
+  #   end
+  # def my_profiles  
+  #   render json: User.profile(user_id: current_user.id)
+  # end
