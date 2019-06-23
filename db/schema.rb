e97a767a2cf3ddd312dc3b86edc213bc5adb0364
Melissa <jstmac@gmail.com>
2019-06-23 17:09:31 -0600

    Deleted Table & changed column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_232319) do
+ActiveRecord::Schema.define(version: 2019_06_23_225939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,6 @@ ActiveRecord::Schema.define(version: 2019_06_19_232319) do
     t.datetime "updated_at", null: false
     t.index ["profile_id"], name: "index_friends_on_profile_id"
     t.index ["user_id"], name: "index_friends_on_user_id"
-  end
-
-  create_table "myspaceusers", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -64,7 +58,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_232319) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "liked_accounts"
+    t.text "follow_friends"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
